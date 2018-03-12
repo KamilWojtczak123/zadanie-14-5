1d283b9b6c15d5797b5352052f3f77b7b2cd9ad5
KamilWojtczak123 <35527613+KamilWojtczak123@users.noreply.github.com>
2018-03-12 11:33:54 +0100

    Update Contact.js

--- components/Contact.js
+++ components/Contact.js
@@ -9,7 +9,7 @@ var Contact = React.createClass({
                 className: 'contactImage',
                 src: 'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'
                 }),
-            React.createElement('p', {className: 'contactLabel'}, 'Imię' + props.item.firstName),
+            React.createElement('p', {className: 'contactLabel'}, 'Imię' + this.props.item.firstName),
             React.createElement('p', {className: 'contactLabel'}, 'Nazwisko' + this.props.item.lastName),
             React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email}, this.props.item.email
                 )
